286af61c9dd59ef0d9d6c6ff19974e9a331b450a
Lucas DiBs <lucas_dibenedetto@hotmail.com>
2022-09-19 19:21:11 -0300

    lalalala

---
+++ cac/src/Components/Button.js
@@ -0,0 +1,14 @@
+import React,{component} from "react";
+import './Components/Button.css';
+
+export default class Button extends component{
+    render(){
+        return (
+            <div>
+            <button classname= "Button">Hello World fron React!</button>
+            </div>
+        )
+
+    }
+
+}
